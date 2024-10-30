c7bfb94b25cd01e0e6f32e4c0ddbdc7d0618a0c9
Joan Orr <joan@joanorr.com>
2024-10-30 11:48:48 +0000

    Add a DNS entry for the wordle bucket.

--- aws/infrastructure/lib/frontend-stack.ts
+++ aws/infrastructure/lib/frontend-stack.ts
@@ -1,11 +1,16 @@
 import * as cdk from 'aws-cdk-lib';
 import * as s3 from 'aws-cdk-lib/aws-s3';
-import * as S3Deployment from 'aws-cdk-lib/aws-s3-deployment';
 import {Construct} from 'constructs';
-
+import * as route53 from 'aws-cdk-lib/aws-route53';
+import * as targets from 'aws-cdk-lib/aws-route53-targets';
+import * as cloudfront from 'aws-cdk-lib/aws-cloudfront';
+import * as origins from 'aws-cdk-lib/aws-cloudfront-origins';
+import * as certificatemanager from 'aws-cdk-lib/aws-certificatemanager';
 
 interface FrontendStackProps extends cdk.StackProps {
   bucketName: string;
+  domainName: string;
+  subdomainName: string;
 }
 
 export class FrontendStack extends cdk.Stack {
@@ -13,18 +18,50 @@ export class FrontendStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props: FrontendStackProps) {
     super(scope, id, props);
 
-    const bucket = new s3.Bucket(this, 'PublicBucket', {
+    const bucket = new s3.Bucket(this, 'Bucket', {
       bucketName: props.bucketName,
       autoDeleteObjects: true,
-      blockPublicAccess: new s3.BlockPublicAccess({
-        blockPublicAcls: false,
-        blockPublicPolicy:false,
-        ignorePublicAcls: false,
-        restrictPublicBuckets: false,
-      }),
-      publicReadAccess: true,
+      blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
       removalPolicy: cdk.RemovalPolicy.DESTROY,
-      websiteIndexDocument: "index.html",
+    });
+
+    // Reference to the pre-existing hosted zone which the API will be bound to
+    const hostedZone = route53.HostedZone.fromLookup(this, 'HostedZone', {
+      domainName: props.domainName,
+    });
+
+    const certificate = new certificatemanager.DnsValidatedCertificate(
+      this, 'Cert', {
+        domainName: props.domainName,
+        hostedZone,
+        region: 'us-east-1',
+        subjectAlternativeNames: [`${props.subdomainName}.${props.domainName}`],
+        validation: {
+          method: certificatemanager.ValidationMethod.DNS,
+          props: {
+            hostedZone: hostedZone,
+          }
+        }
+      });
+
+    const distribution = new cloudfront.Distribution(this, 'CloudFront', {
+      certificate,
+      defaultRootObject: 'index.html',
+      domainNames: [`${props.subdomainName}.${props.domainName}`],
+      defaultBehavior: {
+        origin: origins.S3BucketOrigin.withOriginAccessControl(bucket, {}),
+        allowedMethods: cloudfront.AllowedMethods.ALLOW_GET_HEAD,
+        viewerProtocolPolicy: cloudfront.ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
+      }
+    });
+
+    // Set up a DNS record pointing to the bucket
+    new route53.ARecord(this, 'ARecord', {
+      zone: hostedZone,
+      recordName: props.subdomainName,
+      target: route53.RecordTarget.fromAlias(
+        new targets.CloudFrontTarget(distribution)),
+      ttl: cdk.Duration.seconds(30),
     });
   }
 }
--- aws/infrastructure/lib/pipeline-stack.ts
+++ aws/infrastructure/lib/pipeline-stack.ts
@@ -2,9 +2,12 @@ import * as cdk from 'aws-cdk-lib';
 import {CodeBuildStep, CodePipeline, CodePipelineSource, ShellStep} from 'aws-cdk-lib/pipelines';
 import * as ssm from 'aws-cdk-lib/aws-ssm';
 import {Construct} from 'constructs';
-import { FrontendStack } from './frontend-stack';
+import {FrontendStack} from './frontend-stack';
 import * as iam from 'aws-cdk-lib/aws-iam';
 import * as crypto from 'crypto'
+
+const DOMAIN_NAME =  'webskate101.com';
+
 
 export class PipelineStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -15,7 +18,7 @@ export class PipelineStack extends cdk.Stack {
 
     const pipeline = new CodePipeline(this, 'Pipeline', {
       pipelineName: 'WordlePipeline',
-      synth: new ShellStep('Synth', {
+      synth: new CodeBuildStep('Synth', {
         input: CodePipelineSource.connection('joanorr/simple-wordle', 'main', {
           connectionArn
         }),
@@ -25,6 +28,22 @@ export class PipelineStack extends cdk.Stack {
           'npm ci',
           'npm run build',
           'npx cdk synth',
+        ],
+        rolePolicyStatements: [
+          // This is needed because we are not uploading the cdk.context.json
+          // file to GitHub and so the CodeBuild process needs to be able
+          // to rebuild it.
+          // See: https://docs.aws.amazon.com/cdk/api/v2/docs/aws-cdk-lib.pipelines-readme.html#context-lookups
+          new iam.PolicyStatement({
+            effect: iam.Effect.ALLOW,
+            actions: ['sts:AssumeRole'],
+            resources: ['*'],
+            conditions: {
+              StringEquals: {
+                'aws:ResourceTag/aws-cdk:bootstrap-role': 'lookup',
+              },
+            },
+          }),
         ],
       })
     });
@@ -47,6 +66,8 @@ export class PipelineStack extends cdk.Stack {
       new CreateAppStage(this, 'dev', {
         env: props?.env,
         bucketName: devBucketName,
+        domainName: DOMAIN_NAME,
+        subdomainName: 'wordle-dev',
       }));
     devStageDeployment.addPost(new CodeBuildStep('Deploy to dev', {
       input: buildAppStep,
@@ -72,14 +93,21 @@ export class PipelineStack extends cdk.Stack {
 
 interface CreateAppStageProps extends cdk.StageProps {
   bucketName: string;
+  domainName: string;
+  subdomainName: string;
 }
 
 class CreateAppStage extends cdk.Stage {
 
+
   constructor(scope: Construct, id: string, props: CreateAppStageProps) {
     super(scope, id, props);
 
-    new FrontendStack(this, `Frontend`, {bucketName: props.bucketName});
+    new FrontendStack(this, 'Frontend', {
+      bucketName: props.bucketName,
+      domainName: props.domainName,
+      subdomainName: props.subdomainName,
+    });
   }
 }
 
